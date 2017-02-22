ddab235b48e90e53907e0e5dfbd780a2c3f3712a
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-02-22 23:06:21 +0300

    v0.0.2

--- airtime_player.js
+++ airtime_player.js
@@ -1,8 +1,17 @@
 /* 
-Full customize stream player for Airtime RMS
-Version: 0.0.1
-Author: Oleg Bukatchuk
-E-mail% oleg@bukatchuk.com
+ * Full customize stream player for Airtime RMS
+ * Version: 0.0.2
+ * Author: Oleg Bukatchuk
+ * E-mail% oleg@bukatchuk.com
 */
 
-var stream = 0;
+var stream = 'http://localhost:8000/live320';
+
+checkStream (stream);
+
+function checkStream () {
+    while (stream = '') {
+        document.write('Stream not avalible!');
+    }
+}
+
